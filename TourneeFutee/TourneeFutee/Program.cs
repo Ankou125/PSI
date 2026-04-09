3b82ed0c8bcf85fcced66dcb30ce43ffdfba8900
jeannoux <lefortjeannette99@gmail.com>
2026-04-09 08:56:41 +0200

    clean program

--- TourneeFutee/TourneeFutee/Program.cs
+++ TourneeFutee/TourneeFutee/Program.cs
@@ -4,44 +4,7 @@
     {
         static void Main(string[] args)
         {
-            /*
-            // Création d'une matrice 3x3 avec 0 comme valeur par défaut
-            Matrix m = new Matrix(3, 3, 0);
-
-            Console.WriteLine("Matrice initiale :");
-            m.Print();
-
-            // Modifier quelques valeurs
-            m.SetValue(0, 0, 1);
-            m.SetValue(1, 1, 5);
-            m.SetValue(2, 2, 9);
-
-            Console.WriteLine("\nAprès modification de quelques valeurs :");
-            m.Print();
-
-            // Ajouter une ligne
-            m.AddRow(1);
-            Console.WriteLine("\nAprès ajout d'une ligne à l'indice 1 :");
-            m.Print();
-
-            // Ajouter une colonne
-            m.AddColumn(2);
-            Console.WriteLine("\nAprès ajout d'une colonne à l'indice 2 :");
-            m.Print();
-
-            // Supprimer une ligne
-            m.RemoveRow(0);
-            Console.WriteLine("\nAprès suppression de la ligne 0 :");
-            m.Print();
-
-            // Supprimer une colonne
-            m.RemoveColumn(1);
-            Console.WriteLine("\nAprès suppression de la colonne 1 :");
-            m.Print();
-
-            // Tester GetValue
-            Console.WriteLine("\nValeur en (1,1) : " + m.GetValue(1, 1));
-            */
+        
         }
     }
 }
