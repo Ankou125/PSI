3d7c301cac40186daf321c6fbdb82379f768f59f
Ankou125 <anouk.morand@gmail.com>
2026-04-09 09:27:02 +0200

    Update TourneeFutee2.sql

--- TourneeFutee/TourneeFutee2.sql
+++ TourneeFutee/TourneeFutee2.sql
@@ -30,7 +30,7 @@ CREATE TABLE Sommet (
     graphe_id   INT UNSIGNED    NOT NULL,
     nom         VARCHAR(50)     NOT NULL,               -- nom/label du sommet (ex : "A", "Paris")
     valeur      FLOAT           NULL,                   -- valeur associée au sommet (peut être NULL)
-	indice_mat  INT             
+	indice_mat  INT,             
     -- TODO : ajouter d'autres colonnes si nécessaire
     --        (ex : indice dans la matrice d'adjacence pour faciliter le chargement)
 
